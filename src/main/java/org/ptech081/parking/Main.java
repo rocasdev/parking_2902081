1f2ec7f64e8de8bc17e11ae7eeffabac70d931a8
Andres <afmeza51@misena.edu.co>
2024-02-09 10:27:45 -0500

    actualizacion 1.2

--- src/main/java/org/ptech081/parking/Main.java
+++ src/main/java/org/ptech081/parking/Main.java
@@ -2,6 +2,6 @@ package org.ptech081.parking;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola MI NOMBRE ES DANIEL GALLO!");
+        System.out.println("Hola mucho gusto!");
     }
 }
