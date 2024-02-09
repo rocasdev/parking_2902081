ee64cbc50abab73ec7c9a21605797ebbb0a2adda
Andres <afmeza51@misena.edu.co>
2024-02-09 10:41:03 -0500

    Changes on main.java

--- src/main/java/org/ptech081/parking/Main.java
+++ src/main/java/org/ptech081/parking/Main.java
@@ -2,6 +2,6 @@ package org.ptech081.parking;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola te quiero <3!");
+        System.out.println("Hola mundo!");
     }
 }
